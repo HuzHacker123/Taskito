87c6642269f6a37df6659e1dfef06e8c445fa408
HuzHacker123 <Huzefakazi3131@gmail.com>
2026-04-18 09:55:40 +0530

    Changes for local run

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from app import app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     debug = os.environ.get("FLASK_DEBUG", "false").lower() in ("1", "true", "yes")
-    app.run(host="0.0.0.0", port=port, debug=debug)
+    app.run(host="127.0.0.1", port=port, debug=debug)
